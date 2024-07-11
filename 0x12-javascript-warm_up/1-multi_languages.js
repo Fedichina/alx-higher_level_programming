23e8d80ed12b55a5d0606775368dc5b36ec8224d
Fedichina <fedichinag@gmail.com>
2024-07-11 13:42:30 -0700

    correct

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-let a = "C is fun";
-let b = "Python is cool";
-let c = "JavaScript is amazing";
-console.log(a);
-console.log(b);
-console.log(c);
-
+console.log('C is fun\nPython is cool\nJavascript is amazing');
