32a4d4d84335a57f76f8cb09ed947da53f019e06
Fedichina <fedichinag@gmail.com>
2024-07-22 23:26:37 -0700

    Multi_languages

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+function printCIsFun(x){
+	if(typeOf x !== 'number'){
+		console.log('Missing number of occurrences');
+		retun;
+	}
+	for(x = 0; x < num; x++){
+		console.log('C is fun');
+	}
+}
